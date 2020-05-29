92b541362b7110bab7faffca9321d655f63a498c
muhamood <nmuhamood3@gmail.com>
2020-05-29 11:49:04 +0300

    changes on server

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-const express = require('express');
-const path = require('path');
 const app = require('./app');
-
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res)=>{
-       res.send('hello world!');
-
-});
 
 app.listen(port, ()=> console.log(`Server running on ${port}`));
 
