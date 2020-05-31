42d7f623badf5f7e5fd3089d186309c631fa42b1
muhamood <nmuhamood3@gmail.com>
2020-05-31 18:39:04 +0300

    login user feature

--- src/auth/auth-controller.js
+++ src/auth/auth-controller.js
@@ -1,31 +1,79 @@
-const User = require("../db/user");	
+const jwt = require('jsonwebtoken');
+
+const {SECRET_KEY} = require('../config')
+const User = require("../db/user");
+
+
+
 
 const signUp = async(req, res, next) => {
-	const user = new User({
-		...req.body
-	});
+    const user = new User({
+        ...req.body
+    });
 
-	try{
-		await user.save();
+    try {
+        await user.save();
 
-	return res.send({
-		success: true,
-		message: "SignUp Succesful",
-		user
+        return res.send({
+            success: true,
+            message: "SignUp Succesful",
+            user
 
-	})
+        })
 
-	}catch(e) {
-		console.log(e)
-		return res.send({
-		success: false,
-		message: e.message || "SignUp UnSuccesful"
+    } catch (e) {
 
-	})
-	}}
+        return res.send({
+            success: false,
+            message: e.message || "SignUp UnSuccesful"
 
-	
+        })
+    }
+}
+
+
+const userLogin = async(req, res, next) => {
+
+
+    try {
+
+        const user = await User.findOne({
+            email: req.body.email
+        });
+
+        // check if user then check password
+        if (user && user.password == req.body.password) {
+        	const token = await jwt.sign({id: user._id, email: user.email}, SECRET_KEY);
+            return res.send({
+                success: true,
+                message: "Login Succesful",
+                user,
+                token
+
+            });
+        }
+
+        return res.status(!user ? 404 : 403).send({
+            success: false,
+            message: "Login unSuccesful"
+        });
+
+
+    } catch (e) {
+
+        return res.send({
+            success: false,
+            message: e.message || "Login UnSuccesful"
+        })
+
+    }
+}
+
+// const token = jwt.sign(payload, private, [options, call back]);
+
+
 
 module.exports = {
-		signUp
+    signUp,
+    userLogin
 }
--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,7 @@
 const DATABASE_URL = "MUHAMOOD";
+const SECRET_KEY = "averyprivatekeyfordemo";
 
 module.exports = {
-    DATABASE_URL
+    DATABASE_URL,
+    SECRET_KEY
 }
